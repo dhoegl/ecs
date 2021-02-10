c21a6f9651994b83dcd34cddc8d9c81848d43510
dhoegl <firebird@hoeglund.com>
2021-02-09 22:19:01 -0800

    Ibid

--- includes/menu.php
+++ includes/menu.php
@@ -32,27 +32,27 @@
     }
     if($activeparam == '3' || ($activeparam == '2' && $MyView == 'N'))
     {
-        echo '<li class="nav-item active" id="directory_service"><a class="nav-link" href="ofc_family.php">Directory</a></li>';
+        echo '<li class="nav-item active" id="directory_service"><a class="nav-link" href="family.php">Directory</a></li>';
     }
     else
     {
-        echo '<li class="nav-item" id="directory_service"><a class="nav-link" href="ofc_family.php">Directory</a></li>';
+        echo '<li class="nav-item" id="directory_service"><a class="nav-link" href="family.php">Directory</a></li>';
     }
     if($activeparam == '4')
     {
-        echo '<li class="nav-item active" id="calendar_service"><a class="nav-link" href="ofc_calendar.php">Calendar</a></li>';
+        echo '<li class="nav-item active" id="calendar_service"><a class="nav-link" href="calendar.php">Calendar</a></li>';
     }
     else
     {
-        echo '<li class="nav-item" id="calendar_service"><a class="nav-link" href="ofc_calendar.php">Calendar</a></li>';
+        echo '<li class="nav-item" id="calendar_service"><a class="nav-link" href="calendar.php">Calendar</a></li>';
     }
     if($activeparam == '5')
     {
-        echo '<li class="nav-item active" id="prayer_service"><a class="nav-link" href="ofc_prayer.php">Prayer</a></li>';
+        echo '<li class="nav-item active" id="prayer_service"><a class="nav-link" href="prayer.php">Prayer</a></li>';
     }
     else
     {
-        echo '<li class="nav-item" id="prayer_service"><a class="nav-link" href="ofc_prayer.php">Prayer</a></li>';
+        echo '<li class="nav-item" id="prayer_service"><a class="nav-link" href="prayer.php">Prayer</a></li>';
     }
     if($activeparam == '6')
     {
@@ -65,19 +65,19 @@
     if($_SESSION['reg_admin'] == '1') {
         if($activeparam == '9')
         {
-            echo '<li class="nav-item active"><a class="nav-link" href="ofc_regadmin.php">Registration Admin</a></li>';
+            echo '<li class="nav-item active"><a class="nav-link" href="regadmin.php">Registration Admin</a></li>';
         }
         else {
-            echo '<li class="nav-item"><a class="nav-link" href="ofc_regadmin.php">Registration Admin</a></li>';
+            echo '<li class="nav-item"><a class="nav-link" href="regadmin.php">Registration Admin</a></li>';
         }
     }
     if($_SESSION['pray_admin'] == '1') {
         if($activeparam == '8')
         {
-            echo '<li class="nav-item active"><a class="nav-link" href="ofc_prayeradmin.php">Prayer Admin</a></li>';
+            echo '<li class="nav-item active"><a class="nav-link" href="prayeradmin.php">Prayer Admin</a></li>';
         }
         else {
-            echo '<li class="nav-item"><a class="nav-link" href="ofc_prayeradmin.php">Prayer Admin</a></li>';
+            echo '<li class="nav-item"><a class="nav-link" href="prayeradmin.php">Prayer Admin</a></li>';
         }
     }
     //echo '<li class="nav-item"><a class="nav-link" href="../services/ofc_sendmailtest.php">Test Send Email</a></li>';
