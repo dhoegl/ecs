6c7518431488a906f98cf77da3f414b3e90b0eec
dhoegl <firebird@hoeglund.com>
2021-02-12 21:09:41 -0800

    Ibid

--- services/password_reset_seed.php
+++ services/password_reset_seed.php
@@ -94,8 +94,8 @@ if( isset($_POST[email_address]) && isset($_POST[first_name]) && isset($_POST[la
 else {
     header('Location: //' . $_SESSION['domainname'] . '/welcome.php');
 }
-echo "<script language='javascript'>";
-echo "src='../js/config_ajax_call.js'>;";
-echo "</script>";
+// echo "<script language='javascript'>";
+// echo "src='../js/config_ajax_call.js'>;";
+// echo "</script>";
 
 ?>
