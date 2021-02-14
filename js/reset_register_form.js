f866ff3e00ecc402a390161b7f679557d0cde849
dhoegl <firebird@hoeglund.com>
2021-02-13 16:14:45 -0800

    Register header format update

--- js/reset_register_form.js
+++ js/reset_register_form.js
@@ -1,5 +1,6 @@
 // JavaScript source code
 // If back button used to return to this form, reset all form fields
-$(window).bind("pageshow", function () {
-    $("#register")[0].reset();
+var resetJQ = jQuery.noConflict();
+resetJQ(window).bind("pageshow", function () {
+    resetJQ("#register")[0].reset();
 });
--- register.php
+++ register.php
@@ -26,9 +26,9 @@
   <link href="css/MDBootstrap4191/style.css" rel="stylesheet">
 
     <!--CSS Scripts for Datatables Bootstrap 4 Responsive functions    -->
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+    <!-- <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.6/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.6/css/responsive.bootstrap4.min.css"> -->
 
 
     <!-- Bootstrap 4 BETA CSS -->
