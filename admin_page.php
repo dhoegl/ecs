5e377279cfb8857a95fca6657871205b11f3d7b4
dhoegl <firebird@hoeglund.com>
2021-02-13 21:33:46 -0800

    Ibid

--- admin_page.php
+++ admin_page.php
@@ -3,6 +3,8 @@
 // Last Modified: 2021/02/13
 session_start();
 require_once 'dbconnect.php';
+include('/services/sendmail.php');
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN""http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html lang="en">
@@ -52,13 +54,31 @@ require_once 'dbconnect.php';
             <div class="card bg-light border-primary p-3 mt-2 my-2">
                 <div class="card-body">
                     <div class="card-title font-weight-bold">
-                        This is a Title
+                        Sendmail Testing
                     </div>
                     <div class="card-text font-weight-bold my-2">
-                        This is the body text
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed nisi lacus sed viverra tellus in hac habitasse platea. Est lorem ipsum dolor sit. Tincidunt augue interdum velit euismod in. Nunc eget lorem dolor sed viverra ipsum nunc aliquet. Aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod. Tellus pellentesque eu tincidunt tortor aliquam. Ut tortor pretium viverra suspendisse. Ut diam quam nulla porttitor massa id. Est placerat in egestas erat imperdiet sed euismod. Sit amet est placerat in egestas erat imperdiet sed. Ac tincidunt vitae semper quis. Feugiat sed lectus vestibulum mattis ullamcorper velit sed. Dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu. Lectus sit amet est placerat in egestas.
+                        function sendmail($mailtype, $param1, $param2, $param3, $param4, $param5, $param6) { // params based on each call to sendmail
+                            $mailtype = type of email to send
+                                password_reset
+                                    $param1 = 'Selected' - which family the approved member is being added to
+                                    $param2 = 'Directory' - Registration Admin's idDirectory entry
+                                    $param3 = 'Login' - approved member's user login id
+                                    $param4 = 'FirstName' - approved member's first name
+                                    $param5 = 'LastName' - approved member's last name
+                                    $param6 = 'Email' - approved member's email address
+                                register_request
+                                approved_member
+                                    $param1 = 'Selected' - which family the approved member is being added to
+                                    $param2 = 'Directory' - Registration Admin's idDirectory entry
+                                    $param3 = 'Login' - approved member's user login id
+                                    $param4 = 'FirstName' - approved member's first name
+                                    $param5 = 'LastName' - approved member's last name
+                                    $param6 = 'Email' - approved member's email address
+                                registered_notify
+                                contact_us
                         </p>
+
                     </div>
                 </div>
             </div>
@@ -70,7 +90,6 @@ require_once 'dbconnect.php';
                         This is a Title
                     </div>
                     <div class="card-text font-weight-bold my-2">
-                        This is the body text
                         <p>
                             Mi eget mauris pharetra et ultrices neque ornare aenean. Vel elit scelerisque mauris pellentesque pulvinar. Faucibus a pellentesque sit amet porttitor eget. Convallis convallis tellus id interdum. Cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo. Amet consectetur adipiscing elit duis tristique. Sit amet volutpat consequat mauris nunc congue nisi. Ipsum faucibus vitae aliquet nec ullamcorper sit amet. At lectus urna duis convallis convallis. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et.
                         </p>
@@ -85,7 +104,6 @@ require_once 'dbconnect.php';
                         This is a Title
                     </div>
                     <div class="card-text font-weight-bold my-2">
-                        This is the body text
                         <p>
                             Urna nunc id cursus metus aliquam eleifend. In hendrerit gravida rutrum quisque non tellus orci ac. Faucibus turpis in eu mi. Netus et malesuada fames ac turpis egestas maecenas pharetra. Nulla porttitor massa id neque aliquam vestibulum morbi blandit cursus. Lorem donec massa sapien faucibus et. Urna id volutpat lacus laoreet. Lacinia at quis risus sed vulputate odio. Nisl rhoncus mattis rhoncus urna. Aliquet lectus proin nibh nisl condimentum id. Ipsum nunc aliquet bibendum enim facilisis gravida neque convallis. Sit amet porttitor eget dolor morbi non arcu. Eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada. Neque vitae tempus quam pellentesque nec nam. Porttitor lacus luctus accumsan tortor posuere ac ut consequat. Integer vitae justo eget magna fermentum iaculis eu non diam. Sit amet nisl suscipit adipiscing bibendum est ultricies integer quis. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas.
                         </p>
