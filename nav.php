3e64fdd04b3965225622f61501d34e535539b0df
dhoegl <firebird@hoeglund.com>
2021-02-19 15:06:19 -0800

    Menu bug troubleshoot

--- nav.php
+++ nav.php
@@ -1,5 +1,5 @@
 <!-- Navbar-->
-<nav class="navbar fixed-top navbar-expand-lg navbar-light" id="headercolor headerforecolor">
+<nav class="navbar fixed-top navbar-expand-lg navbar-light" id="headercolor">
 <a class="navbar-brand" href="welcome.php">
         <img id="nav_logo" width="30" height="30" class="d-inline-block align-top" alt="Logo" />
         <span id="navbar_brand"></span>
