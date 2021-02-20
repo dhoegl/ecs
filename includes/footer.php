2793dfa013abb252b5b691f854c279d0d5980a28
dhoegl <firebird@hoeglund.com>
2021-02-19 17:54:12 -0800

    Ibid2

--- includes/footer.php
+++ includes/footer.php
@@ -7,7 +7,7 @@
       <div class="col-12">
         <!--Copyright-->
         <p class="text-center">Copyright © 2021:
-          <a href="//ecs.ourfamilyconnections.org"><strong> OurFamilyConnections.org</strong></a>
+          <a href="//ecs.ourfamilyconnections.org" id="footercolor"><strong> OurFamilyConnections.org</strong></a>
         </p>
       </div>
       <!-- Grid column -->
