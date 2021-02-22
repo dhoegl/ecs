092e75604348500e1ba5f4e50adeddc63ac47667
dhoegl <firebird@hoeglund.com>
2021-02-21 22:15:38 -0800

    Ibid11

--- admin_page.php
+++ admin_page.php
@@ -287,7 +287,7 @@ adminjQ(document).ready(function () {
         });
             // The ajax call succeeded. 
             email_send.done(function (data, textStatus, jqXHR) {
-                alert( "Send Email ajax call Success: " + textStatus);
+                alert( "Send Email ajax call Success: " + data.Mailtype);
             });
             // The ajax call failed. 
             email_send.fail(function (jqXHR, textStatus, errorThrown) {
