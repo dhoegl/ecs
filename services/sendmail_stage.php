ddd8eb7e034b6e0c445ec52fc5c3085ff161a076
dhoegl <firebird@hoeglund.com>
2021-02-25 21:05:10 -0800

    Ibid21

--- services/sendmail_stage.php
+++ services/sendmail_stage.php
@@ -4,6 +4,9 @@
 // session_start();
 // require_once 'dbconnect.php';
 // include('services/sendmail.php');
+echo "<script language='javascript'>";
+echo "console.log('Arrived at sendmail_stage script = " . $sendmail_stage . "');";
+echo "</script>";
 
 ?>
 
