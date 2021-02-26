2b48c3b6f6b82a2f4c562d59660e7a53c48039b7
dhoegl <firebird@hoeglund.com>
2021-02-26 14:47:46 -0800

    Ibid5

--- services/ajax_update_new_registrant.php
+++ services/ajax_update_new_registrant.php
@@ -1,3 +1,10 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN""http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html lang="en">
+  <head>
+  </head>
+  <body>
+  HI
 <?php
 //New Registrant Accept script
 //Called from regadmin.php
@@ -79,3 +86,5 @@ echo "</script>";
         echo $response;
     }
 ?>
+</body>
+</html>
