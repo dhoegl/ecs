3fdf7f944692a6f825087d4ed6ae66dde1502de3
dhoegl <firebird@hoeglund.com>
2021-02-25 20:42:31 -0800

    Ibid16

--- services/sendmail_stage.php
+++ services/sendmail_stage.php
@@ -23,30 +23,29 @@ include('services/sendmail.php');
     <input type="hidden" class="form-control hidden_params" id="headercolorvalue" name="headercolorname"></input> 
     <input type="hidden" class="form-control hidden_params" id="headerforecolorvalue" name="headerforecolorname"></input> 
 
-
 <!-- **************************** Get the DOM text of hidden parameters ******************** -->
 <script type="text/javascript">
-var adminjQ = jQuery.noConflict();
-adminjQ(document).ready(function () {
-    var mailtype = "";
-    var paramcheck2 = "";
-    var customer = "";
-    var domain = "";
-    var headercolorvalue = "";
-    var headerforecolorvalue = "";
-    var $mailtype = "";
-    var $customer = "";
-    var $domain = "";
-    var $headercolor  = "";
-    var $headerforecolor = "";
-    var $family_select = "";
-    var $admin_dir = "";
-    var $login = "";
-    var $first = "";
-    var $last = "";
-    var $email = "";
-    var $reset = "";
-    function sendmail_stage($mailtype, $customer, $domain, $headercolor, $headerforecolor, $family_select, $admin_dir, $login, $first, $last, $email, $reset) { // params based on each call to sendmail
+// var adminjQ = jQuery.noConflict();
+// adminjQ(document).ready(function () {
+//     var mailtype = "";
+//     var paramcheck2 = "";
+//     var customer = "";
+//     var domain = "";
+//     var headercolorvalue = "";
+//     var headerforecolorvalue = "";
+//     var $mailtype = "";
+//     var $customer = "";
+//     var $domain = "";
+//     var $headercolor  = "";
+//     var $headerforecolor = "";
+//     var $family_select = "";
+//     var $admin_dir = "";
+//     var $login = "";
+//     var $first = "";
+//     var $last = "";
+//     var $email = "";
+//     var $reset = "";
+//     function sendmail_stage($mailtype, $customer, $domain, $headercolor, $headerforecolor, $family_select, $admin_dir, $login, $first, $last, $email, $reset) { // params based on each call to sendmail
 // function sendmail_setup($mailtype, $customer, $domain, $headercolor, $headerforecolor, $family_select, $admin_dir, $login, $first, $last, $email, $reset) { // params based on each call to sendmail
     //$mailtype = type of email to send
     //$customer = 'Customer Name' - Name of church/school (email banner)
@@ -60,44 +59,44 @@ adminjQ(document).ready(function () {
     //$last/$LastName2 = 'LastName' - approved member's last name
     //$email/$Email2 = 'Email' - approved member's email address
     //$reset = 'ResetKey' - key credential for password reset
-    domain = adminjQ("#domainname").text();
-    console.log("Domain = " + domain);
-    customer = adminjQ("#custname").text();
-    console.log("Customer = " + customer);
-    headercolorvalue = adminjQ("#headercolorvalue").text();
-    console.log("headercolorvalue = " + headercolorvalue);
-    headerforecolorvalue = adminjQ("#headerforecolorvalue").text();
-    console.log("headerforecolorvalue = " + headerforecolorvalue);
+    // domain = adminjQ("#domainname").text();
+    // console.log("Domain = " + domain);
+    // customer = adminjQ("#custname").text();
+    // console.log("Customer = " + customer);
+    // headercolorvalue = adminjQ("#headercolorvalue").text();
+    // console.log("headercolorvalue = " + headercolorvalue);
+    // headerforecolorvalue = adminjQ("#headerforecolorvalue").text();
+    // console.log("headerforecolorvalue = " + headerforecolorvalue);
            
-                    var email_send = adminjQ.ajax({
-                        url: 'services/sendmail.php',
-                        type: "POST",
+                    // var email_send = adminjQ.ajax({
+                    //     url: 'services/sendmail.php',
+                    //     type: "POST",
                         // dataType: 'json',
-                        data: {
-                            "Mailtype": $mailtype,
-                            "Domain": domain,
-                            "Customer": customer,
-                            "HeaderColor": headercolorvalue,
-                            "Headerforecolorvalue": headerforecolorvalue,
-                            "Family": $family_select,
-                            "Admin": $admin_dir,
-                            "Login": $login,
-                            "First": $first,
-                            "Last": $last,
-                            "Email": $email,
-                            "ResetKey": $reset
-                        },
-                    });
+                    //     data: {
+                    //         "Mailtype": $mailtype,
+                    //         "Domain": domain,
+                    //         "Customer": customer,
+                    //         "HeaderColor": headercolorvalue,
+                    //         "Headerforecolorvalue": headerforecolorvalue,
+                    //         "Family": $family_select,
+                    //         "Admin": $admin_dir,
+                    //         "Login": $login,
+                    //         "First": $first,
+                    //         "Last": $last,
+                    //         "Email": $email,
+                    //         "ResetKey": $reset
+                    //     },
+                    // });
                         // The ajax call succeeded. 
-                        email_send.done(function (data, textStatus, jqXHR) {
-                            alert( "Send Email ajax call Success: " + data);
-                        });
+                        // email_send.done(function (data, textStatus, jqXHR) {
+                        //     alert( "Send Email ajax call Success: " + data);
+                        // });
                         // The ajax call failed. 
-                        email_send.fail(function (jqXHR, textStatus, errorThrown) {
-                        alert( "Send Email ajax call Fail: " + errorThrown);
-                        });
+                        // email_send.fail(function (jqXHR, textStatus, errorThrown) {
+                        // alert( "Send Email ajax call Fail: " + errorThrown);
+                        // });
             
-                };
+                // };
             
                     // sendmail(mailtype, customer, domain, headercolorvalue, headerforecolorvalue, login, first, last, email)
             
@@ -106,8 +105,8 @@ adminjQ(document).ready(function () {
                         // console.log("Customer Name = " + customer);
                         // domain = paramcheck.find("#domainname").text();
                         // console.log("Domain Name = " + domain);
-            })
-            </script>
+            // })
+</script>
 
 </body>
 </html>
