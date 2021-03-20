03174be24d8f3645cac99f3ec8c8afa1af2014a9
dhoegl <firebird@hoeglund.com>
2021-03-19 22:47:10 -0700

    Ibid

--- services/simplexml.php
+++ services/simplexml.php
@@ -1,11 +1,11 @@
 <?php
 if (file_exists("../_tenant/Config_simpletest.xml")) {
     $xml = simplexml_load_file("../_tenant/Config_simpletest.xml");
-    $themename = $xml->name;
+    $themename = $xml->settings->customer->name;
     // $themename = "Weird that xml doesn't work";
-    $themedomain = $xml->domain;
-    $themetitle = $xml->hometitle;
-    $themecolor = $xml->banner_color;
+    $themedomain = $xml->settings->customer->domain;
+    $themetitle = $xml->settings->customer->hometitle;
+    $themecolor = $xml->settings->customer->banner_color;
 
 echo $themename;
 echo $themedomain;
