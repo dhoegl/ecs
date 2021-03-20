be808c0807f5edaf65de25bd152d908c6e15a3f1
dhoegl <firebird@hoeglund.com>
2021-03-19 23:06:42 -0700

    Ibid7

--- services/simplexml.php
+++ services/simplexml.php
@@ -1,20 +1,20 @@
 <?php
-if (file_exists("Config_simpletest.xml")) {
-    $xml = simplexml_load_file("Config_simpletest.xml");
-    // $themename = $xml->settings->customer->name;
-    // $themedomain = $xml->settings->customer->domain;
-    // $themetitle = $xml->settings->customer->hometitle;
-    // $themecolor = $xml->settings->customer->banner_color;
+if (file_exists("../_tenant/Config_simpletest.xml")) {
+    $xml = simplexml_load_file("../_tenant/Config_simpletest.xml");
+    $themename = $xml->settings->customer->name;
+    $themedomain = $xml->settings->customer->domain;
+    $themetitle = $xml->settings->customer->hometitle;
+    $themecolor = $xml->settings->customer->banner_color;
 
-// echo $themename;
-// echo $themedomain;
-// echo $themetitle;
-// echo $themecolor;
-echo '<pre>';
-print_r($xml);
+echo $themename;
+echo $themedomain;
+echo $themetitle;
+echo $themecolor;
+// echo '<pre>';
+// print_r($xml);
 }
 else {
-    echo "Nothing to show here";
+    echo "Can't find ../_tenant/Config_simpletest.xml";
 }
 
 ?>
