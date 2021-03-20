41035b2cad7235b357a9664b7d948752f90dbe1e
dhoegl <firebird@hoeglund.com>
2021-03-19 17:16:55 -0700

    Ibid5

--- services/recover_submit.php
+++ services/recover_submit.php
@@ -19,8 +19,8 @@ if(isset($_POST['password_reset']))
 // Extract email theme elements from config.xml
 if (file_exists("../_tenant/Config_simpletest.xml")) {
     $xml = simplexml_load_file("../_tenant/Config_simpletest.xml");
-    // $themename = $xml->name;
-    $themename = "Weird that xml doesn't work";
+    $themename = $xml->name;
+    // $themename = "Weird that xml doesn't work";
     $themedomain = $xml->domain;
     $themetitle = $xml->hometitle;
     $themecolor = $xml->banner_color;
