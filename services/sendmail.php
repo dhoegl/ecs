4b0226a71d7f0b9d2742c4bfc74d15351b76a8a3
dhoegl <firebird@hoeglund.com>
2021-03-23 20:38:10 -0700

    Ibid3

--- services/sendmail.php
+++ services/sendmail.php
@@ -9,6 +9,8 @@
 // }
 require_once('../dbconnect.php');
 // include('../includes/event_logs_update.php');
+    // Event Log  trap
+    require_once('../includes/event_logs_update.php');
 $text = array();
 
 // function sendmail($mailtype, $customer, $domain, $headercolor, $headerforecolor, $family_select, $admin_dir, $login, $first, $last, $username, $email, $reset) 
