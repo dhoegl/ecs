b62833e31f178911b56f45efa24111aa19616d87
dhoegl <firebird@hoeglund.com>
2021-03-25 19:53:56 -0700

    Ibid4

--- services/ajax_update_new_registrant.php
+++ services/ajax_update_new_registrant.php
@@ -91,11 +91,7 @@ echo "</script>";
             $regacceptlogin = $mysql->query($regacceptloginquery) or die("A database error occurred when trying to update new Registrant info into Login table. See ajax_update_new_registrant.php. Error:" . $mysql->errno . " : " . $mysql->error);
             // eventLogUpdate('admin_update', "Admin ID: " . $_SESSION['idDirectory'], "Registrant Approve", "LoginID: " . $Login2 . " to Directory entry: " . $Selected2);
             // Send Registration Approval to handler at reg_approve_submit_to_sendmail.js
-            echo "
-            <script type='text/javascript'>
-            regapprovenotify('$Email2', '$FirstName2', '$LastName2', '$user_name', '$Login2', '$themename', '$themedomain', '$themetitle', '$themecolor', '$themeforecolor');
-            </script>
-            ";
+            regapprovenotify($Email2, $FirstName2, $LastName2, $user_name, $Login2, $themename, $themedomain, $themetitle, $themecolor, $themeforecolor);
         $response = "success_entry_to_existing_family";
         // $text[] = array('Status' => 'Accept Success');
 	    // header('Content-type: application/json');
