f405805a456237746f4a08f97e03cc9b791deb7e
dhoegl <firebird@hoeglund.com>
2021-03-30 20:27:31 -0700

    Ibid13

--- services/test_sendmail_stage_old.php
+++ services/test_sendmail_stage_old.php
@@ -6,7 +6,7 @@ echo "alert('Arrived at test_sendmail_stage');";
 echo "</script>";
 require($_SERVER["DOCUMENT_ROOT"] . '/dbconnect.php');
 // include($_SERVER["DOCUMENT_ROOT"] . '/includes/event_logs_update.php');
-include('../services/sendmail.php');
+// include('../services/sendmail.php');
 echo "<script language='javascript'>";
 echo "alert('Got past Sendmail include');";
 echo "</script>";
@@ -28,10 +28,41 @@ echo "</script>";
         $Email = "firebird@hoeglund.com";
         $reset = "";
         $text = array();
-        regsendmailnotify('approved_member', $Email, $First, $Last, $user_name, $LoginID, $themename, $themedomain, $themetitle, $themecolor, $themeforecolor);
-    // sendmail_stage('approved_member', $customer, $domain, $headercolor, $headerforecolor, $family_select, $admin_dir, $Login2, $FirstName2, $LastName2, $Email2, $reset);
-    // echo "<script language='javascript'>";
-    // echo "alert('Reached the end of test_sendmail_stage - Errors?');";
-    // echo "</script>";
+        // regsendmailnotify('approved_member', $Email, $First, $Last, $user_name, $LoginID, $themename, $themedomain, $themetitle, $themecolor, $themeforecolor);
+
+        $mailtype = 'approved_member';
+        $domain = $themedomain;
+        $customer = $themename;
+        $title = $themetitle;
+        $headercolorvalue = $themecolor;
+        $headerforecolorvalue = $themeforecolor;
+        // $family_select = $_POST['Family']; //family select for new registrants (possibly unused for email comms)
+        // $admin_dir = $_POST['Admin']; //Administrator's Directory ID (possibly unused for email comms)
+        $login = $LoginID; //UserName
+        $firstname = $First;
+        $lastname = $Last;
+        $username = $user_name;
+        $email = $Email;
+    
+        // Sendmail copy into this PHP flow...
+        $maillink = $domain;
+        $mailto = $email;
+        $mailsubject = "Approved access to the " . $customer . " family directory" . "\n..";
+        $mailmessage = "<html><body>";
+        $mailmessage .= "<p>(This was sent from an unmonitored mailbox)</p>";
+        $mailmessage .= "<p style='background-color: " .  $headercolorvalue . "; font-size: 30px; font-weight: bold; color: " . $headerforecolorvalue . "; padding: 25px; width=100%;'>";
+        $mailmessage .= $customer . "</p>";
+        $mailmessage .= "<p>Hello <strong>" . $firstname . " " . $lastname . "</strong></p>";
+        $mailmessage .= "<p>You have been approved to access " . $customer . "'s directory site!</p>";
+        $mailmessage .= "<p>Click on the link below to login<br /></p>";
+        $mailmessage .= "<p><a href=http://" . $maillink . ">" . $customer . "</a></p>";
+        $mailmessage .= "<p><br />Thank you!<br />The OurFamilyConnections team.</p>";            
+        $mailmessage .= "</body></html>";
+        $mailfrom = "noreply@ourfamilyconnections.org";
+        $mailheaders = "From:" . $mailfrom . "\r\n";
+        $mailheaders .= "Reply-To:" . $mailfrom . "\r\n";
+        $mailheaders .= "MIME-Version: 1.0\r\n";
+        $mailheaders .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+        mail($mailto,$mailsubject,$mailmessage,$mailheaders);
     
 ?>
