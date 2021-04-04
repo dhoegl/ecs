409e2e6f93ff1327e8c04f44ec1d805bbf2812f8
dhoegl <firebird@hoeglund.com>
2021-04-03 22:25:03 -0700

    Ibid5

--- includes/footer.php
+++ includes/footer.php
@@ -12,7 +12,8 @@
         <p>Copyright © 2021:<strong> OurFamilyConnections.org</strong></p>
       </div>
       <div class="col-4">
-        <p class="text-center"><a href="../policies/Privacy.pdf" target="_blank">Privacy Policy</a></p>
+        <p class="text-center">Privacy Policy</p>
+        <!-- <p class="text-center"><a href="../policies/Privacy.pdf" target="_blank">Privacy Policy</a></p> -->
       </div>
       <!-- Grid column -->
       <!-- <div class="col-md-5 col-lg-4 ml-lg-0">
