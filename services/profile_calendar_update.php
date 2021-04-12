d5fce0934c2de25f40f14b56b445489f1c736bdd
dhoegl <firebird@hoeglund.com>
2021-04-11 22:06:35 -0700

    EventLog Updates Profile calendar,contact,children

--- services/profile_calendar_update.php
+++ services/profile_calendar_update.php
@@ -6,7 +6,7 @@ if(!$_SESSION['logged in']) {
 }
 
 require_once('../dbconnect.php');
-//include_once('/includes/event_logs_update.php');
+include_once('../includes/event_logs_update.php');
 
 
 
@@ -19,7 +19,7 @@ if(isset($_POST['submitcalendar']))
 	$child_name = $_POST['lastname'];
 	$calendarupdatequery = "UPDATE " . $_SESSION['dirtablename'] . " SET Anniv_Date = '" . $my_anniv . "', BDay_1_Date = '" . $his_bday . "', BDay_2_Date = '" . $her_bday . "'  WHERE idDirectory = '". $_SESSION["Famview_Profile"] . "'"; 
 	$calendarupdate = $mysql->query($calendarupdatequery) or die("A database error occurred when trying to update calendar info. See ofc_profile_calendar_update.php. Error : " . mysql_errno() . mysql_error());		
-//	eventLogUpdate('profile_update', $namelast, 'Profile Update : Calendar : DirectoryID = ', $_SESSION['idDirectory']);
+	eventLogUpdate('profile_update', 'Anniv_Date = ' . $my_anniv . ', BDay_1_Date = ' . $his_bday . ', BDay_2_Date = ' . $her_bday, 'Profile Update-Calendar' , 'username= ' . $_SESSION['username']);
 }
 else {
 	echo "isset didn't work";
